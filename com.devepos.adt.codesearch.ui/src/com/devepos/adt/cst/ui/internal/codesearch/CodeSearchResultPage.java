c0d33ad5d8536032eae4404cc4258f9243a19053
stockbal <stocki87@gmail.com>
2022-01-31 21:08:13 +0100

    feat(search-result): handle match count display on collection nodes

--- com.devepos.adt.codesearch.ui/src/com/devepos/adt/cst/ui/internal/codesearch/CodeSearchResultPage.java
+++ com.devepos.adt.codesearch.ui/src/com/devepos/adt/cst/ui/internal/codesearch/CodeSearchResultPage.java
@@ -29,9 +29,10 @@ import org.eclipse.ui.dialogs.PreferencesUtil;
 
 import com.devepos.adt.base.IAdtObjectTypeConstants;
 import com.devepos.adt.base.ui.AdtBaseUIResources;
+import com.devepos.adt.base.ui.ContextHelper;
 import com.devepos.adt.base.ui.IAdtBaseImages;
 import com.devepos.adt.base.ui.IGeneralCommandConstants;
-import com.devepos.adt.base.ui.StylerFactory;
+import com.devepos.adt.base.ui.IGeneralContextConstants;
 import com.devepos.adt.base.ui.action.ActionFactory;
 import com.devepos.adt.base.ui.action.CommandFactory;
 import com.devepos.adt.base.ui.search.ISearchResultPageExtension;
@@ -39,27 +40,154 @@ import com.devepos.adt.base.ui.tree.IAdtObjectReferenceNode;
 import com.devepos.adt.base.ui.tree.ICollectionTreeNode;
 import com.devepos.adt.base.ui.tree.IStyledTreeNode;
 import com.devepos.adt.base.ui.tree.ITreeNode;
-import com.devepos.adt.base.ui.tree.LoadingTreeItemsNode;
 import com.devepos.adt.base.ui.util.AdtTypeUtil;
 import com.devepos.adt.cst.ui.internal.preferences.CodeSearchPreferencesPage;
 import com.sap.adt.tools.core.model.adtcore.IAdtCoreFactory;
 import com.sap.adt.tools.core.model.adtcore.IAdtObjectReference;
 import com.sap.adt.tools.core.ui.navigation.AdtNavigationServiceFactory;
 
+/**
+ * Result page of a ABAP Code Search query
+ *
+ * @author Ludwig Stockbauer-Muhr
+ *
+ */
 public class CodeSearchResultPage extends AbstractTextSearchViewPage implements
     ISearchResultPageExtension<CodeSearchQuery> {
 
   private IStructuredContentProvider contentProvider;
   private IAction openPreferencesAction;
+  private ContextHelper contextHelper;
 
   public CodeSearchResultPage() {
     super();
   }
 
+  private class MatchViewerUpdater {
+    private Object[] updatedMatchNodes;
+    private CodeSearchResult result;
+    private Set<ICollectionTreeNode> parentNodesWithoutMatches;
+    private Set<ICollectionTreeNode> nodesToRefresh;
+    private boolean isRootNodeAdjusted;
+
+    public MatchViewerUpdater(final Object[] updatedMatchNodes) {
+      this.updatedMatchNodes = updatedMatchNodes;
+      result = (CodeSearchResult) getInput();
+      parentNodesWithoutMatches = new HashSet<>();
+      nodesToRefresh = new HashSet<>();
+    }
+
+    public void update() {
+      handleChangedElements();
+      removeNodesWithoutChildren();
+      refreshViewer();
+    }
+
+    private void handleChangedElements() {
+      for (Object obj : updatedMatchNodes) {
+        // check if element still has matches
+        if (getDisplayedMatchCount(obj) <= 0) {
+          ITreeNode matchNode = (ITreeNode) obj;
+          ICollectionTreeNode parentNode = matchNode.getParent();
+          if (parentNode != null) {
+            result.removeSearchResult(matchNode);
+            parentNode.removeChild(matchNode);
+            parentNode.setNodeValue(null);
+            parentNodesWithoutMatches.add(parentNode);
+          }
+        }
+      }
+    }
+
+    private void refreshViewer() {
+      StructuredViewer viewer = getViewer();
+      if (viewer instanceof TableViewer) {
+        viewer.refresh();
+        return;
+      }
+      updateMatchCounts();
+
+      if (!isRootNodeAdjusted && !nodesToRefresh.isEmpty()) {
+        for (ICollectionTreeNode node : nodesToRefresh) {
+          viewer.refresh(node);
+        }
+      } else {
+        // refresh all nodes
+        viewer.refresh();
+      }
+    }
+
+    private void removeNodesWithoutChildren() {
+      // check parents if they still have children
+      for (ICollectionTreeNode parent : parentNodesWithoutMatches) {
+
+        while (parent != null) {
+          // check if we removed a node from the root node, then we have to refresh the
+          // whole viewer as the root node is not visible
+          if (!isRootNodeAdjusted && parent == result.getResultTree()) {
+            isRootNodeAdjusted = true;
+          }
+
+          if (parent.hasChildren()) {
+            nodesToRefresh.add(parent);
+            break;
+          }
+
+          // maybe the node was added in an earlier iteration where it still had children
+          nodesToRefresh.remove(parent);
+
+          ICollectionTreeNode child = parent;
+          parent = parent.getParent();
+
+          if (parent != null) {
+            // remove the child
+            parent.removeChild(child);
+          }
+        }
+      }
+    }
+
+    private void updateAndPropagateMatchCount(final ICollectionTreeNode collectionNode) {
+      if (collectionNode == null) {
+        return;
+      }
+      if (collectionNode.getNodeValue() == null) {
+        collectionNode.setNodeValue(collectionNode.getChildren().size());
+        updateAndPropagateMatchCount(collectionNode.getParent());
+      } else {
+        int matchCount = 0;
+        for (ITreeNode childNode : collectionNode.getChildren()) {
+          matchCount += (Integer) childNode.getNodeValue();
+        }
+        collectionNode.setNodeValue(matchCount);
+        updateAndPropagateMatchCount(collectionNode.getParent());
+      }
+    }
+
+    private void updateMatchCounts() {
+      Set<ICollectionTreeNode> tmpNodes = new HashSet<>();
+
+      for (ICollectionTreeNode node : nodesToRefresh) {
+        updateAndPropagateMatchCount(node);
+
+        if (!isRootNodeAdjusted) {
+          ICollectionTreeNode parent = node.getParent();
+          while (parent != null) {
+            if (parent.getParent() != null) {
+              tmpNodes.add(parent);
+            }
+            parent = parent.getParent();
+          }
+        }
+      }
+      nodesToRefresh.addAll(tmpNodes);
+    }
+  }
+
   /**
    * Custom view label provider for the Result Tree
    *
-   * @author stockbal
+   * @author Ludwig Stockbauer-Muhr
    */
   private static class ViewLabelProvider extends LabelProvider implements ILabelProvider,
       IStyledLabelProvider {
@@ -91,18 +219,22 @@ public class CodeSearchResultPage extends AbstractTextSearchViewPage implements
           text = new StyledString();
         }
       } else {
-        if (element instanceof LoadingTreeItemsNode) {
-          text.append(searchResult.getDisplayName(), StylerFactory.ITALIC_STYLER);
-          return text;
-        }
         if (element instanceof IAdtObjectReferenceNode) {
           text.append(searchResult.getDisplayName());
           String type = ((IAdtObjectReferenceNode) element).getAdtObjectType();
-          if (!type.equals(IAdtObjectTypeConstants.CLASS_INCLUDE)) {
-            String typeDescription = AdtTypeUtil.getInstance().getTypeDescription(type);
-            if (typeDescription != null) {
-              text.append(" (" + typeDescription + ")", StyledString.QUALIFIER_STYLER);
-            }
+          String typeDescription = null;
+          if (IAdtObjectTypeConstants.BUSINESS_OBJ_TYPE_PROGRAM.equals(type)) {
+            typeDescription = "Program";
+          } else if (!IAdtObjectTypeConstants.CLASS_INCLUDE.equals(type)) {
+            typeDescription = AdtTypeUtil.getInstance().getTypeDescription(type);
+          }
+          if (typeDescription != null) {
+            text.append(" (" + typeDescription + ")", StyledString.QUALIFIER_STYLER);
+          }
+          Integer matchCount = (Integer) searchResult.getNodeValue();
+          if (matchCount != null) {
+            text.append(String.format(" %d %s", matchCount, matchCount > 1 ? "Matches" : "Match"),
+                StyledString.COUNTER_STYLER);
           }
         }
       }
@@ -123,6 +255,18 @@ public class CodeSearchResultPage extends AbstractTextSearchViewPage implements
     super.createControl(parent);
 
     initializeActions();
+
+    contextHelper = ContextHelper.createForServiceLocator(getSite());
+    contextHelper.activateAbapContext();
+    contextHelper.activateContext(IGeneralContextConstants.SEARCH_PAGE_VIEWS);
+  }
+
+  @Override
+  public void dispose() {
+    if (contextHelper != null) {
+      contextHelper.deactivateAllContexts();
+    }
+    super.dispose();
   }
 
   @Override
@@ -161,64 +305,7 @@ public class CodeSearchResultPage extends AbstractTextSearchViewPage implements
 
   @Override
   protected void elementsChanged(final Object[] updatedElements) {
-    Set<ICollectionTreeNode> parentNodesWithoutMatches = new HashSet<>();
-    Set<ITreeNode> nodesToRefresh = new HashSet<>();
-    CodeSearchResult result = (CodeSearchResult) getInput();
-
-    for (Object obj : updatedElements) {
-      // check if element still has matches
-      if (getDisplayedMatchCount(obj) <= 0) {
-        ITreeNode matchNode = (ITreeNode) obj;
-        ICollectionTreeNode parentNode = matchNode.getParent();
-        if (parentNode != null) {
-          result.removeSearchResult(matchNode);
-          parentNodesWithoutMatches.add(parentNode);
-        }
-      }
-    }
-
-    boolean isRootNodeAdjusted = false;
-
-    // check parents if they still have children
-    for (ICollectionTreeNode parent : parentNodesWithoutMatches) {
-
-      while (parent != null) {
-        // check if we removed a node from the root node, then we have to refresh the
-        // whole viewer as the root node is not visible
-        if (!isRootNodeAdjusted && parent == result.getResultTree()) {
-          isRootNodeAdjusted = true;
-        }
-
-        if (parent.hasChildren()) {
-          nodesToRefresh.add(parent);
-          break;
-        }
-
-        // maybe the node was added in an earlier iteration where it still had children
-        nodesToRefresh.remove(parent);
-
-        ICollectionTreeNode child = parent;
-        parent = parent.getParent();
-
-        if (parent != null) {
-          parent.removeChild(child);
-        }
-      }
-    }
-
-    StructuredViewer viewer = getViewer();
-    if (viewer instanceof TableViewer) {
-      viewer.refresh();
-    } else {
-      if (!isRootNodeAdjusted && !nodesToRefresh.isEmpty()) {
-        for (Object node : nodesToRefresh) {
-          viewer.refresh(node);
-        }
-      } else {
-        // refresh all nodes
-        viewer.refresh();
-      }
-    }
+    new MatchViewerUpdater(updatedElements).update();
   }
 
   @Override
@@ -268,8 +355,9 @@ public class CodeSearchResultPage extends AbstractTextSearchViewPage implements
     if (element instanceof IAdtObjectReferenceNode) {
       adtObjRef = ((IAdtObjectReferenceNode) element).getObjectReference();
     } else if (element instanceof SearchMatchNode) {
+      SearchMatchNode matchNode = (SearchMatchNode) element;
       adtObjRef = IAdtCoreFactory.eINSTANCE.createAdtObjectReference();
-      adtObjRef.setUri(((SearchMatchNode) element).getUri());
+      adtObjRef.setUri(matchNode.getUri());
     }
 
     if (adtObjRef != null) {
